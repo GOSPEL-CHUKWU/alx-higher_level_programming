b6da07ec2def201fa7ea50a49d766fedcfdd7467
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-03-28 13:29:06 -0700

    omoo

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,8 @@ A Square Class
 
 
 class Square:
-
+     """This is a Square Class that returns the size"""
+     
     def __init__(self, size=0):
         """__init__
         The __init__ method initializes the size value of the square.
