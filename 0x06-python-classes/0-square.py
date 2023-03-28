badef15fc123a846b0fb4cdf48afd3177f3f9269
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-03-28 11:35:08 -0700

    early bug 😫

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
-"""#!/usr/bin/python3
-Square = __import__('0-square').Square
+#!/usr/bin/python3
+
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+"""Square = __import__('0-square').Square
 
 my_square = Square()
 print(type(my_square))
