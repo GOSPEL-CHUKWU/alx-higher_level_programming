b39ca2c3f3f771f8bc782dce787e0c50bea10817
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-03-28 11:55:04 -0700

    push

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Square Class
+This class don't do anything for the moment
+"""
 
-"""This is an Square Class Module"""
 
 class Square:
-    """This is an empty class and a class is like a blueprint"""
-    pass;
+    pass
