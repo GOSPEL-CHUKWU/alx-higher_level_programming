98e95c91ac371a40a89896e450473039df5c7065
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-03-28 13:33:50 -0700

    I think I fixed it

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 """Square Class
 This is a Square Class
 """
 
 class Square:
     """This is a Square Class that returns the size"""
+    
     def __init__(self,size=0):
         """__init__
         The __init__ method initializes the size value of the square.
