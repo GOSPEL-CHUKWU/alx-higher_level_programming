63d25e0ee309b52369b30c9aa5e4fd5de80f9f33
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-03-28 11:48:48 -0700

    error fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
 """python3 -c 'print(__import__("my_module").__doc__)'"""
-"""Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)"""
 
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-    """It acts like a blueprint of the square"""
+    
     pass;
